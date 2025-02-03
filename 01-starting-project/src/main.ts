82b06d888e4348b3b63c49533a8149104c511664
Vicgital <Vicgital@hotmail.com>
2025-02-03 07:15:10 -0600

    Angular Essentials part 1

---
+++ 01-starting-project/src/main.ts
@@ -0,0 +1,8 @@
+import { bootstrapApplication } from '@angular/platform-browser';
+
+import { AppComponent } from './app/app.component';
+import { ApplicationConfig } from '@angular/core';
+
+bootstrapApplication(AppComponent).catch((err) => console.error(err));
+
+
